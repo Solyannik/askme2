ab66fd268b256cd73e73c795f340f9cdea539c84
Solyannik Olga <solyannikolga@gmail.com>
2020-12-24 16:15:57 +0200

    Add photos to events

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,35 @@
 module ApplicationHelper
   def user_avatar(user)
-    if user.avatar?
-      user.avatar.url
+    return user.avatar.url if user.avatar?
+
+    asset_pack_path("media/images/user.png")
+  end
+
+  def user_avatar_thumb(user)
+    if user.avatar.file.present?
+      user.avatar.thumb.url
     else
-      'user.png'
+      asset_pack_path('media/images/user.png')
+    end
+  end
+
+  def event_photo(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.url
+    else
+      asset_pack_path('media/images/event.jpg')
+    end
+  end
+
+  def event_thumb(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.thumb.url
+    else
+      asset_pack_path('media/images/event_thumb.jpg')
     end
   end
 
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,8 +3,15 @@ class Event < ApplicationRecord
   has_many :comments
   has_many :subscriptions
   has_many :subscribers, through: :subscriptions, source: :user
+  has_many :photos, dependent: :destroy
 
   validates :title, presence: true, length: {maximum: 255}
   validates :address, presence: true
-  validates :datetime, presence: true
+  validates :datetime, presence: true, inclusion: {in: Date.current.. }, inclusion: {in: Time.now.. }
+
+  scope :persisted, -> { where 'id IS NOT NULL' }
+
+  def visitors
+    (subscribers + [user]).uniq
+  end
 end
