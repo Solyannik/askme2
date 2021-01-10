a30b7c9e1c7d32bbf789e0d42ee148f43a77ebe1
Solyannik <solyannikolga@gmail.com>
2021-01-10 13:48:13 +0200

    change production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,4 +118,3 @@ end
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-end
