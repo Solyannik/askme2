2a7118b9d75972c949336b1d8e61efeed52f1caa
Solyannik <solyannikolga@gmail.com>
2021-03-21 13:39:09 +0200

    change event.rb

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,7 +7,7 @@ class Event < ApplicationRecord
 
   validates :title, presence: true, length: {maximum: 255}
   validates :address, presence: true
-  validates :datetime, presence: true, inclusion: {in: Date.current.. }, inclusion: {in: Time.now.. }
+  validates :datetime, presence: true, inclusion: {in: Date.current.. }, inclusion: {in: Time.now.. }` `
 
   scope :persisted, -> { where 'id IS NOT NULL' }
 
