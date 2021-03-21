92343589919369c167bd7a9b18bb8395e03e8eaa
Solyannik <solyannikolga@gmail.com>
2021-03-21 14:08:48 +0200

    add storage.yml and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_24_101320) do
+ActiveRecord::Schema.define(version: 2021_03_19_231027) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
