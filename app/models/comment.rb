443cb77853f81b401dbb89722f3ff4defa45f87b
Solyannik <solyannikolga@gmail.com>
2021-03-22 19:23:48 +0200

    notify subscribers when post comment and photo

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :event
-  belongs_to :user
+  belongs_to :user, optional: true
 
   validates  :event, presence: true
   validates :body, presence: true
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,6 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :letter_opener
- config.action_controller.asset_host = "http://localhost:3000"
+  config.action_controller.asset_host = "http://localhost:3000"
   config.action_mailer.asset_host = "http://localhost:3000"
 end
